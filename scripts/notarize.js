21d94cdff5b79d114d171a83c8c48d57938b50be
Rob McVey <rmcvey@netflix.com>
2022-05-31 23:10:24 -0700

    Made app id configurable

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -10,7 +10,7 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: 'com.robmcvey.gmaildesktop',
+    appBundleId: process.env.APPID,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
